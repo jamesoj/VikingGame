db532329110f3d060577a98d7e92d4c0578195cf
jamesoj <james22nethercott@gmail.com>
2020-10-23 00:47:21 +0100

    Commit 3
    collision and stats

---
+++ Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour
+{
+    public CharacterStats characterStats;
+    public float currentHealth;
+    public float maxHealth;
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.currentHealth = this.maxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
